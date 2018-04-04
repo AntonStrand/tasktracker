20aa5686668a9372a015c538c810c1b4c4ea6475
antonstrand <jag@antonstrand.se>
2018-04-04 12:37:03 +0200

    Return an array of response

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const User = require('./../models/User')
+const { findErrorMessages } = require('./../utils/controllerHelpers')
+
+// getErrorMessages :: ValidationError -> [String] null
+const getErrorMessages = findErrorMessages(
+  key => key === 'username' || key === 'password'
+)
 
 const signUp = repository => (req, res, next) =>
   repository
@@ -10,10 +16,14 @@ const signUp = repository => (req, res, next) =>
         password: req.body.password
       })
     )
-    .then(user => res.status(200))
+    .then(user => {
+      res.status(200)
+      res.send([`User '${user.username}' has been created.`])
+    })
     .catch(err => {
+      console.log(err)
       res.status(422)
-      res.send(err.message)
+      res.send(getErrorMessages(err))
     })
 
 module.exports = {
