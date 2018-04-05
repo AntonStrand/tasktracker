f1936cf9970ce3eed9b035d66521e395cc92e710
antonstrand <jag@antonstrand.se>
2018-04-05 15:57:59 +0200

    Creating a seperate test file for sign up

---
+++ backend/test/signUp.test.js
@@ -0,0 +1,89 @@
+const { describe, it } = require('mocha')
+const expect = require('chai').expect
+const signUp = require('./../src/controllers/authentication/signUp')
+
+const createRepository = save => ({ save })
+
+describe('signUp()', () => {
+  const req = {
+    body: {
+      username: 'Username',
+      password: 'Password'
+    }
+  }
+
+  it('Should call repository.save()', done => {
+    try {
+      let isCalled = false
+
+      const repository = createRepository(user => {
+        isCalled = true
+        return Promise.resolve({ username: 'Username' })
+      })
+
+      const res = {
+        send: x => x,
+        status: status => status
+      }
+
+      signUp(repository)(req, res)
+      expect(isCalled).to.equal(true)
+      done()
+    } catch (error) {
+      done(error)
+    }
+  })
+
+  it('Should send status to 200 if successful', done => {
+    const repository = createRepository(user => Promise.resolve({}))
+    const res = {
+      send: x => x,
+      status: status => {
+        try {
+          expect(status).to.equal(200)
+          done()
+        } catch (error) {
+          done(error)
+        }
+      }
+    }
+    signUp(repository)(req, res)
+  })
+
+  it('Should send message in Array if successful', done => {
+    const repository = createRepository(user => Promise.resolve({}))
+    const res = {
+      send: x => {
+        try {
+          expect(Array.isArray(x)).to.equal(true)
+          expect(typeof x[0]).to.equal('string')
+          done()
+        } catch (error) {
+          done(error)
+        }
+      },
+      status: status => status
+    }
+    signUp(repository)(req, res)
+  })
+
+  it('Should send status to 422 if invalid input', done => {
+    const repository = createRepository(() =>
+      Promise.reject(new Error('error'))
+    )
+
+    const res = {
+      send: x => x,
+      status: status => {
+        try {
+          expect(status).to.equal(422)
+          done()
+        } catch (error) {
+          done(error)
+        }
+      }
+    }
+
+    signUp(repository)(req, res)
+  })
+})
