5db895e5b9243ce57b0d58d27fac78ccc8fab40e
antonstrand <jag@antonstrand.se>
2018-04-05 23:52:36 +0200

    Removing express-session since jwt is used instead

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
-const session = require('express-session')
 
 module.exports = () => {
   const app = express()
@@ -21,26 +20,6 @@ module.exports = () => {
   // Parse body
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-
-  // Set session options.
-  const sessionOptions = {
-    name: 'session',
-    secret: 'fghufoisyrgvhe8gy3wun::jgskafeigjso3267548923',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 // 1 day
-    }
-  }
-
-  // Set cookie to secure when in production.
-  if (app.get('env') === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sessionOptions.cookie.secure = true // serve secure cookies
-  }
-
-  app.use(session(sessionOptions))
 
   return app
 }
