225f34115f0aff5a362aa4aa36f9ff653028965b
antonstrand <jag@antonstrand.se>
2018-04-05 15:42:35 +0200

    Login user using session

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const bcrypt = require('bcrypt')
 const User = require('./../models/User')
 const { getErrorMessages } = require('./../utils/controllerHelpers')
 
@@ -8,6 +9,13 @@ const getMessages = getErrorMessages(
   key => key === 'username' || key === 'password'
 )
 
+/**
+ * Try to register a user.
+ * @param {Object} repository
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
 const signUp = repository => (req, res, next) =>
   repository
     .save(
@@ -25,6 +33,37 @@ const signUp = repository => (req, res, next) =>
       res.send(getMessages(err))
     })
 
+// handleLoginSuccess :: Object -> Object -> User -> Undefined
+const handleLoginSuccess = (req, res, user) => {
+  console.log('login')
+  req.session.user = sessionUser(user)
+  res.status(200).send('You just logged in.')
+}
+
+// sessionUser :: User -> Object
+const sessionUser = user => ({
+  id: user._id,
+  username: user.username
+})
+
+/**
+ * Try to log in user.
+ * @param {Object} repository
+ * @param {Object} req
+ * @param {Object} res
+ */
+const login = repository => async (req, res) => {
+  try {
+    const user = await repository.findUserByName(req.body.username)
+    ;(await bcrypt.compare(req.body.password, user.password))
+      ? handleLoginSuccess(req, res, user)
+      : console.log('was not found')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 module.exports = {
-  signUp
+  signUp,
+  login
 }
