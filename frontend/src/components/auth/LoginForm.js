cfee8edd20b2717799f949b03b9eadbebddba198
antonstrand <jag@antonstrand.se>
2018-04-06 11:44:18 +0200

    Sending in the submit function to the form

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,9 +1,18 @@
 import Form from './Form'
 import { defaultProps } from 'recompose'
+import axios from 'axios'
+
+const onSubmit = user => evt => {
+  evt.preventDefault()
+  axios
+    .post('api/login', user)
+    .then(console.log)
+    .catch(console.log)
+}
 
 const LoginForm = defaultProps({
   submitLabel: 'Login',
-  url: '/login'
+  onSubmit
 })(Form)
 
 export default LoginForm
