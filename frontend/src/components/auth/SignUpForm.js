c928a4d39632930a19b71b06b586664105be4791
antonstrand <jag@antonstrand.se>
2018-04-06 11:46:41 +0200

    Update the sign up form to match the new form functionality

--- frontend/src/components/auth/SignUpForm.js
+++ frontend/src/components/auth/SignUpForm.js
@@ -1,9 +1,18 @@
 import Form from './Form'
 import { defaultProps } from 'recompose'
+import axios from 'axios'
+
+const onSubmit = user => evt => {
+  evt.preventDefault()
+  axios
+    .post('api/sign-up', user)
+    .then(console.log)
+    .catch(console.log)
+}
 
 const SignUpFrom = defaultProps({
   submitLabel: 'Sign up',
-  url: '/sign-up'
+  onSubmit
 })(Form)
 
 export default SignUpFrom
