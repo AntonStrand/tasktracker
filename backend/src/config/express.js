062adfb69ec0bf15edb213ee49c40134dd4a24c5
antonstrand <jag@antonstrand.se>
2018-04-07 12:01:55 +0200

    changing security settings to exlude it from being a problem

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -1,28 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const helmet = require('helmet')
+// const helmet = require('helmet')
 const path = require('path')
 
 module.exports = baseURL => {
   const app = express()
 
-  // Using helmet for setting headers to improve security.
-  app.use(helmet())
+  // // Using helmet for setting headers to improve security.
+  // app.use(helmet())
 
-  // Set CSP. This is not included in helmet().
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'", 'fonts.googleapis.com', 'fonts.gstatic.com']
-      }
-    })
-  )
+  // // Set CSP. This is not included in helmet().
+  // app.use(
+  //   helmet.contentSecurityPolicy({
+  //     directives: {
+  //       defaultSrc: ["'self'", 'fonts.googleapis.com', 'fonts.gstatic.com']
+  //     }
+  //   })
+  // )
+
+  app.use(express.static(path.join(baseURL, '../../frontend/build')))
 
   // Parse body
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-
-  app.use(express.static(path.join(baseURL, '../../frontend/build')))
 
   return app
 }
