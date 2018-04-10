ac867c8f3ded36e02c6a5daec98aa015401648a7
antonstrand <jag@antonstrand.se>
2018-04-10 20:53:12 +0200

    Adding an middleware handiling socket communication

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,4 +1,16 @@
-import { createStore } from 'redux'
+/**
+ * Initialize redux store.
+ */
+
+import { createStore, applyMiddleware } from 'redux'
+import createSocketIoMiddleware from 'redux-socket.io'
+import io from 'socket.io-client'
 import authReducer from './reducers/authentication'
 
-export default createStore(authReducer)
+const socket = io()
+const socketMiddleware = createSocketIoMiddleware(socket, 'ws/')
+
+export default applyMiddleware(socketMiddleware)(createStore)(
+  authReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
