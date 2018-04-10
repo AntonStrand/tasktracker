9e2021e304a48ecddc5d71da7864eee6589c3c4e
antonstrand <jag@antonstrand.se>
2018-04-10 10:53:50 +0200

    Create user authenticated action

---
+++ frontend/src/actions/types.js
@@ -0,0 +1,1 @@
+export const USER_AUTHENTICADED = 'user:AUTHENTICADED'
---
+++ frontend/src/actions/userAuthenticated.js
@@ -0,0 +1,6 @@
+import { USER_AUTHENTICADED } from './types'
+
+export default token => ({
+  type: USER_AUTHENTICADED,
+  token
+})
