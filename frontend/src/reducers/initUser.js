538bc73c822368131ffcd8df0abe530a6e51443c
antonstrand <jag@antonstrand.se>
2018-04-11 13:23:05 +0200

    Fixing init state structure

--- frontend/src/reducers/initUser.js
+++ frontend/src/reducers/initUser.js
@@ -2,7 +2,11 @@ import { INIT_USER_STATE } from './../actions/types'
 
 const initialState = {
   authToken: null,
-  user: null
+  user: {
+    username: null,
+    projects: null,
+    assignedTasks: null
+  }
 }
 
 // initUser :: State -> Action -> State
