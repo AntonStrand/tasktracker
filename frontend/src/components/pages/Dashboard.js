3c588b76c8dc9e65cf43756d1cc8148261b342f0
antonstrand <jag@antonstrand.se>
2018-04-11 13:28:04 +0200

    Showing username when logged in

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
-const Dashboard = () => <h1>Dashboard</h1>
+const Dashboard = props => <h1>Dashboard {props.name}</h1>
 
-export default Dashboard
+const mapToProps = state => ({ name: state.user.username })
+
+export default connect(mapToProps)(Dashboard)
+
+Dashboard.propTypes = {
+  name: PropTypes.string
+}
