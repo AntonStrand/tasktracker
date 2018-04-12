fe447215e0be71707ebef2aa3154d4c4818deee8
antonstrand <jag@antonstrand.se>
2018-04-12 23:31:50 +0200

    Project model

---
+++ backend/src/models/Project.js
@@ -0,0 +1,37 @@
+/**
+ * Mongoose model for Project.
+ */
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Missing project title']
+  },
+  description: { type: String },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  updatedAt: { type: Date },
+  deadline: { type: Date },
+  members: {
+    type: [Schema.Types.ObjectId],
+    required: [true, 'A project has to have at least one member']
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'On going'
+  },
+  tasks: {
+    type: [Schema.Types.ObjectId]
+  },
+  totalTime: { type: Number, default: 0 },
+  tags: { type: [String] }
+})
+
+module.exports = mongoose.model('Project', schema)
