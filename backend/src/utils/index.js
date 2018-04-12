d026ea68564191140aafdbca5bb86060a28a9763
antonstrand <jag@antonstrand.se>
2018-04-12 23:21:26 +0200

    A util function for filter async

--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -6,7 +6,17 @@ const isNotNil = R.complement(R.isNil)
 // alwaysFalse :: a -> Boolean False
 const alwaysFalse = R.always(false)
 
+// filterAsync :: pred (a -> Boolean) -> [a] -> [a]
+const filterAsync = R.curry(async (pred, xs) => {
+  const filtered = []
+  for (let i = 0; i < xs.length; i++) {
+    if (await pred(xs[i])) filtered.push(xs[i])
+  }
+  return filtered
+})
+
 module.exports = {
   isNotNil,
-  alwaysFalse
+  alwaysFalse,
+  filterAsync
 }
