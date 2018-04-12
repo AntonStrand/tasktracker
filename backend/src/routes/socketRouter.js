d1f20b4142fcca0c64fd33a374bdca8053bd6135
antonstrand <jag@antonstrand.se>
2018-04-12 23:41:40 +0200

    Refactoring for better readability

--- backend/src/routes/socketRouter.js
+++ backend/src/routes/socketRouter.js
@@ -1,13 +1,12 @@
 const project = require('./../controllers/projectController')
 const projectRepository = require('./../repositories/projectRepository')
 
+const actions = type =>
+  ({
+    'ws/CREATE_NEW_PROJECT': project.create(projectRepository)
+  }[type])
+
 module.exports = io =>
   io.on('connection', socket =>
-    socket.on('action', payload => {
-      switch (payload.type) {
-        case 'ws/CREATE_NEW_PROJECT':
-          project.create(projectRepository)(payload)
-          break
-      }
-    })
+    socket.on('action', payload => actions(payload.type)(payload))
   )
