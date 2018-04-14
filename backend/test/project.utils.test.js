01d71053025093f96c66c27e6609bebad8e52317
antonstrand <jag@antonstrand.se>
2018-04-14 23:17:38 +0200

    Testing saveProjectToMembers()

--- backend/test/project.utils.test.js
+++ backend/test/project.utils.test.js
@@ -15,6 +15,28 @@ const testFns = require('./../src/controllers/project/utils')
 // createProjectDoc
 
 describe('Project - Utils', () => {
+  describe('saveProjectToMembers()', () => {
+    it('should call addProject with the project id and username of each member', done => {
+      const _id = '123'
+      const members = ['user1', 'user2', 'user3']
+      const result = []
+      const repository = {
+        addProject: (username, id) => {
+          result.push({ username, id })
+
+          if (result.length === members.length) {
+            result.forEach(({ username, id }, i) => {
+              expect(username).to.equal(members[i])
+              expect(id).to.equal(_id)
+            })
+            done()
+          }
+        }
+      }
+      testFns.saveProjectToMembers(repository)({ members, _id })
+    })
+  })
+
   describe('onlyLowercaseLetters()', () => {
     it('should convert capital letters to lowercase', () => {
       const testString = 'ALLCAPS'
