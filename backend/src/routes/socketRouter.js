17fbed5d975b0f1e4245be9e662ec5e96ecf7d9a
antonstrand <jag@antonstrand.se>
2018-04-20 17:57:21 +0200

    Logging socket.id

--- backend/src/routes/socketRouter.js
+++ backend/src/routes/socketRouter.js
@@ -8,6 +8,8 @@ const actions = type =>
   }[type])
 
 module.exports = io =>
-  io.on('connection', socket =>
+  io.on('connection', socket => {
+    // TODO: Save socket.id to user to be able to add users to the project.
+    console.log('socket.id:', socket.id)
     socket.on('action', payload => actions(payload.type)(socket, payload))
-  )
+  })
