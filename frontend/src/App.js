7291312d25317c6fa1cd6e4500132be87dbc18dc
antonstrand <jag@antonstrand.se>
2018-04-22 17:37:25 +0200

    Adding route for projects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './components/auth/LoginForm'
 import Dashboard from './components/pages/Dashboard'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PrivateRoute from './containers/PrivateRoute'
+import Project from './components/pages/Project'
 
 const App = () => (
   <Router>
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path='/' component={SignUpForm} />
         <Route path='/login' component={LoginForm} />
         <PrivateRoute path='/dashboard' component={Dashboard} />
+        <PrivateRoute path='/project/:id' component={Project} />
       </Switch>
     </div>
   </Router>
