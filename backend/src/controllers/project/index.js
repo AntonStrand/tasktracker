33b47d6a7d1bd822cd587c14bbceffe8d03aa69c
antonstrand <jag@antonstrand.se>
2018-04-24 15:50:12 +0200

    Adding addTask and dependency injection

--- backend/src/controllers/project/index.js
+++ backend/src/controllers/project/index.js
@@ -1,3 +1,8 @@
+const projectRepository = require('./../../repositories/projectRepository')
+const userRepository = require('./../../repositories/userRepository')
+const taskRepository = require('./../../repositories/taskRepository')
+
 module.exports = {
-  create: require('./create').create
+  create: require('./create').create(projectRepository, userRepository),
+  addTask: require('./addTask').addTask(projectRepository, taskRepository)
 }
