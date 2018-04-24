d6674f09d33bc4c2ca002010c12021be56400e1d
antonstrand <jag@antonstrand.se>
2018-04-24 13:20:34 +0200

    Using mongoose built in timestamp instead of having to do it manually

--- backend/src/models/Project.js
+++ backend/src/models/Project.js
@@ -5,36 +5,33 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const schema = new Schema({
-  title: {
-    type: String,
-    required: [true, 'Missing project title.']
+const schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Missing project title.']
+    },
+    description: { type: String },
+    deadline: { type: Date },
+    members: {
+      type: [String],
+      validate: [
+        tags => tags.length > 0,
+        'A project has to have at least one member.'
+      ]
+    },
+    status: {
+      type: String,
+      required: true,
+      default: 'On going'
+    },
+    tasks: {
+      type: [Schema.Types.ObjectId]
+    },
+    totalTime: { type: Number, default: 0 },
+    tags: { type: [String] }
   },
-  description: { type: String },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-  updatedAt: { type: Date },
-  deadline: { type: Date },
-  members: {
-    type: [String],
-    validate: [
-      tags => tags.length > 0,
-      'A project has to have at least one member.'
-    ]
-  },
-  status: {
-    type: String,
-    required: true,
-    default: 'On going'
-  },
-  tasks: {
-    type: [Schema.Types.ObjectId]
-  },
-  totalTime: { type: Number, default: 0 },
-  tags: { type: [String] }
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Project', schema)
