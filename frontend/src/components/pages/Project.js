6d58165c44cac96c444d10c29ece95422aa21bcb
antonstrand <jag@antonstrand.se>
2018-04-30 15:26:45 +0200

    Starting with layout

--- frontend/src/components/pages/Project.js
+++ frontend/src/components/pages/Project.js
@@ -3,16 +3,48 @@ import TaskField from './../form/TaskField'
 import { connect } from 'react-redux'
 import { getProjectTasks } from './../../selectors'
 import TaskListItem from './../Task/TaskListItem'
+import styled from 'styled-components'
+import ProgressBar from './../gui/ProgressBar'
 
-const Project = ({ project, tasksById }) =>
-  console.log(getProjectTasks(tasksById)) || (
-    <div>
-      {getProjectTasks(tasksById).map((task, key) => (
-        <TaskListItem task={task} key={key} />
-      ))}
-      <TaskField parent={{ type: 'project', id: project.id }} />
-    </div>
-  )
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  text-align: left;
+  padding: 0 3em;
+  box-sizing: border-box;
+`
+
+const Header = styled.div`
+  margin-bottom: 1.5em;
+`
+
+const Title = styled.h1`
+  display: inline-block;
+`
+
+const TotalTime = styled.span`
+  float: right;
+`
+
+const Project = ({ project, tasksById }) => (
+  <Wrapper>
+    <Header>
+      <Title>{project.title}</Title>
+      <TotalTime>
+        <Title>{project.totalTime}</Title>
+      </TotalTime>
+      <ProgressBar style={{ borderRadius: '1em', height: '4px' }} />
+    </Header>
+    <TaskField
+      style={{ maxWidth: '20em' }}
+      parent={{ type: 'project', id: project.id }}
+    />
+    {getProjectTasks(tasksById).map((task, key) => (
+      <TaskListItem task={task} key={key} />
+    ))}
+  </Wrapper>
+)
 
 const mapToProps = (state, props) => ({
   project: state.projects.projectsById[props.match.params.id],
