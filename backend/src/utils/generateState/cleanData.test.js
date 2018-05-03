64d89c44c5fc96ce70573bf99a51ec864d393400
antonstrand <jag@antonstrand.se>
2018-05-03 22:48:42 +0200

    Testing structure

--- backend/src/utils/generateState/cleanData.test.js
+++ backend/src/utils/generateState/cleanData.test.js
@@ -3,14 +3,20 @@ const expect = require('chai').expect
 const cleanData = require('./cleanData')
 
 describe('cleanData()', () => {
-  it('should return an object', () => {
+  it('should return an object with "id" and "_doc" should be flatten', () => {
     const testData = {
       _id: '123',
       _doc: {
         title: 'Test title'
       }
     }
+
+    const expectedResult = {
+      id: '123',
+      title: 'Test title'
+    }
     const result = cleanData(testData)
     expect(result).to.be.an('object')
+    expect(result).to.deep.equal(expectedResult)
   })
 })
