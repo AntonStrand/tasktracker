e484a659a27031e52445f81ff53c6cdd2d5e4802
antonstrand <jag@antonstrand.se>
2018-05-04 17:39:25 +0200

    It's better if the functions throws errors in case the arguments is invalid

--- backend/src/utils/generateState/projectState/index.js
+++ backend/src/utils/generateState/projectState/index.js
@@ -8,21 +8,23 @@ const indexProjects = reduce(
 )
 
 // getIndexLength :: Object -> Number
-const getIndexLength = index =>
-  index && typeof index === 'object' ? Object.keys(index).length : 0
+const getIndexLength = index => Object.keys(index).length
+
+const indexToState = index => ({
+  projectsById: index,
+  count: getIndexLength(index)
+})
 
 // createProjectState :: repository -> User -> {ProjectState}
 const createProjectState = (projectRepo, user) =>
   getCleanedProjects(projectRepo, user)
     .then(indexProjects)
-    .then(index => ({
-      projectsById: index,
-      count: getIndexLength(index)
-    }))
+    .then(indexToState)
     .catch(() => Error('An error occured while fetching your projects.'))
 
 module.exports = {
   indexProjects,
   getIndexLength,
+  indexToState,
   createProjectState
 }
--- backend/src/utils/generateState/projectState/projectState.test.js
+++ backend/src/utils/generateState/projectState/projectState.test.js
@@ -1,6 +1,11 @@
 const { describe, it } = require('mocha')
 const expect = require('chai').expect
-const { indexProjects, getIndexLength, createProjectState } = require('./index')
+const {
+  indexProjects,
+  getIndexLength,
+  indexToState,
+  createProjectState
+} = require('./index')
 
 describe('projectState', () => {
   describe('indexProjects()', () => {
@@ -45,21 +50,59 @@ describe('projectState', () => {
       expect(result).to.equal(expectedResult)
     })
 
-    it('should return 0 if the input is null', () => {
+    it('should throw an Error if argument is null', () => {
       const testData = null
-
-      const expectedResult = 0
-
-      const result = getIndexLength(testData)
-      expect(result).to.equal(expectedResult)
+      try {
+        getIndexLength(testData)
+      } catch (error) {
+        expect(error).to.not.equal(null)
+      }
     })
-    it('should return 0 if the input is a String', () => {
-      const testData = 'null'
 
-      const expectedResult = 0
-
-      const result = getIndexLength(testData)
-      expect(result).to.equal(expectedResult)
+    it('should throw an Error if argument is a String', () => {
+      const testData = 'String'
+      try {
+        getIndexLength(testData)
+      } catch (error) {
+        expect(error).to.not.equal(null)
+      }
     })
   })
+
+  describe('indexToState', () => {
+    it('should take project index and turn it into a project state', () => {
+      const testData = {
+        '1': { id: '1', title: 'Project 1' },
+        '2': { id: '2', title: 'Project 2' },
+        '3': { id: '3', title: 'Project 3' }
+      }
+
+      const expectedResult = {
+        projectsById: testData,
+        count: 3
+      }
+
+      const result = indexToState(testData)
+      expect(result).to.deep.equal(expectedResult)
+    })
+    it('should throw an error in case it argument is not an Object', () => {
+      const testData = null
+      try {
+        indexToState(testData)
+      } catch (error) {
+        expect(error).to.not.equal(null)
+      }
+    })
+  })
+
+  // describe('createProjectState', () => {
+  //   it(
+  //     "should return an Object containing the number of projects and it's data", () => {
+  //       const projectRepo = {
+  //         findById: id => ({})
+  //       }
+  //     }
+  //   )
+  //   it('should throw an Error in case something goes wrong')
+  // })
 })
