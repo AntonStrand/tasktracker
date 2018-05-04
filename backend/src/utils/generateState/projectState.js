75944f7e6187d0b7cfb5bb791b2704b7fd99212b
antonstrand <jag@antonstrand.se>
2018-05-04 12:54:59 +0200

    Removed getCleanedProjects since it has it's own module

--- backend/src/utils/generateState/projectState.js
+++ backend/src/utils/generateState/projectState.js
@@ -1,24 +1,14 @@
 const reduce = require('ramda').reduce
-const cleanData = require('./cleanData')
+const getCleanedProjects = require('./getCleanedProjects')
 
 // indexProjects :: [Project] -> { project.id: Project }
 const indexProjects = reduce(
-  (index, project) => ({ ...index, [project['id']]: project }),
+  (index, project) => ({ ...index, [project.id]: project }),
   {}
 )
 
 // getIndexLength :: Object -> Number
 const getIndexLength = index => Object.keys(index).length
-
-const getCleanedProjects = (projectRepo, user) =>
-  Promise.all(
-    user.projects.map(id =>
-      projectRepo
-        .findById(id)
-        .then(cleanData)
-        .catch(() => 'An error occured while fetching this project.')
-    )
-  )
 
 // createProjectState :: repository -> User -> {ProjectState}
 const createProjectState = (projectRepo, user) =>
@@ -28,7 +18,7 @@ const createProjectState = (projectRepo, user) =>
       projectsById: index,
       count: getIndexLength(index)
     }))
-    .catch(() => 'An error occured while fetching your projects.')
+    .catch(() => Error('An error occured while fetching your projects.'))
 
 module.exports = {
   createProjectState
