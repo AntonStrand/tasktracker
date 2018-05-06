82ca010422365c18ad9612b139e2e455e6b86ba8
antonstrand <jag@antonstrand.se>
2018-05-06 19:19:21 +0200

    Testing arrayToIndex()

--- backend/test/project.utils.test.js
+++ backend/test/project.utils.test.js
@@ -192,4 +192,21 @@ describe('Project - Utils', () => {
       expect(result).to.equal(expectedResult)
     })
   })
+
+  describe('arrayToIndex()', () => {
+    it('should turn an array of objects to one object grouped by the "id" key', () => {
+      const testData = [
+        { id: '123', title: 'title1' },
+        { id: '456', title: 'title2' },
+        { id: '789', title: 'title3' }
+      ]
+      const expectedResult = {
+        '123': { id: '123', title: 'title1' },
+        '456': { id: '456', title: 'title2' },
+        '789': { id: '789', title: 'title3' }
+      }
+      const result = testFns.arrayToIndex(testData)
+      expect(result).to.deep.equal(expectedResult)
+    })
+  })
 })
