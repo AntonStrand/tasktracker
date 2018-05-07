0564742dcadbcb811c4b6e365d0c9e329d0df43c
antonstrand <jag@antonstrand.se>
2018-05-07 22:06:21 +0200

    Remove console.log

--- frontend/src/actions/project.js
+++ frontend/src/actions/project.js
@@ -5,9 +5,8 @@ const extractFormValues = form =>
   form.reduce((res, field) => ({ ...res, [field.name]: field.value }), {})
 
 // createProject :: JWT-token, Object -> Action
-export const createProject = (token, formState) =>
-  console.log(extractFormValues(formState)) || {
-    type: CREATE_NEW_PROJECT,
-    token,
-    formData: extractFormValues(formState)
-  }
+export const createProject = (token, formState) => ({
+  type: CREATE_NEW_PROJECT,
+  token,
+  formData: extractFormValues(formState)
+})
