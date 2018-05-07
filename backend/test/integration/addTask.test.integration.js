bde0a6b4c4e9d3ddfdef3e8c058f91ab61274fc1
antonstrand <jag@antonstrand.se>
2018-05-07 16:08:19 +0200

    Testing for failures

--- backend/test/integration/addTask.test.integration.js
+++ backend/test/integration/addTask.test.integration.js
@@ -21,23 +21,27 @@ describe('Add Task - Integration', () => {
   // Setup
   beforeEach(done => {
     // Start application
-    taskDB.remove({}, () => {
-      userRepository
-        .save({
-          username: 'user',
-          password: '1234'
-        })
-        .then(() =>
-          axios
-            .post('http://localhost:8080/api/login', {
+    userDB
+      .remove({}, () =>
+        taskDB.remove({}, () =>
+          userRepository
+            .save({
               username: 'user',
               password: '1234'
             })
-            .then(({ data }) => (token = data.user.token))
+            .then(() =>
+              axios
+                .post('http://localhost:8080/api/login', {
+                  username: 'user',
+                  password: '1234'
+                })
+                .then(({ data }) => (token = data.user.token))
+            )
+            .then(() => done())
+            .catch(done)
         )
-        .then(() => done())
-        .catch(done)
-    })
+      )
+      .catch(done)
   })
 
   after(() => {
@@ -59,7 +63,6 @@ describe('Add Task - Integration', () => {
         taskRepository
           .findById(message.task.id)
           .then(task => {
-            console.log(task)
             expect(task.title).to.equal(message.task.title)
             expect(task.parent.id.toString()).to.equal(message.task.parent.id)
             client.disconnect()
@@ -74,6 +77,59 @@ describe('Add Task - Integration', () => {
         formData: {
           parent: { type: 'project', id: '5aec8ff37577706238f89340' },
           taskName: 'new task'
+        }
+      })
+    })
+  })
+  it('should emit a ACCESS_DENIED and not add a task if username is invalid', done => {
+    const client = io.connect('http://localhost:8080', options)
+    client.on('connect', function () {
+      client.on('action', function (message) {
+        expect(message.type).to.equal('user/ACCESS_DENIED')
+        expect(message.message).to.equal('Access denied.')
+        taskRepository
+          .findAll()
+          .then(tasks => {
+            expect(tasks.length).to.equal(0)
+            client.disconnect()
+            done()
+          })
+          .catch(done)
+      })
+
+      client.emit('action', {
+        type: 'ws/CREATE_NEW_TASK',
+        token: 'invalid',
+        formData: {
+          parent: { type: 'project', id: '5aec8ff37577706238f89340' },
+          taskName: 'new task'
+        }
+      })
+    })
+  })
+  it('should emit a VALIDATION_ERROR and not add a task if task data is invalid', done => {
+    const client = io.connect('http://localhost:8080', options)
+    client.on('connect', function () {
+      client.on('action', function (message) {
+        expect(message.type).to.equal('form/VALIDATION_ERROR')
+        expect(message.formId).to.equal('task')
+        expect(message.error).to.equal("Sorry, the task couldn't be added.")
+        taskRepository
+          .findAll()
+          .then(tasks => {
+            expect(tasks.length).to.equal(0)
+            client.disconnect()
+            done()
+          })
+          .catch(done)
+      })
+
+      client.emit('action', {
+        type: 'ws/CREATE_NEW_TASK',
+        token,
+        formData: {
+          parent: { type: 'project', id: '5aec8ff37577706238f89340' },
+          taskName: { title: '12345678' }
         }
       })
     })
