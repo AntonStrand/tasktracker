50ef6a08eb5ac67d3c40a3ce213bca052b3f73c2
antonstrand <jag@antonstrand.se>
2018-05-09 00:17:37 +0200

    Adding a default project to 'existing'

--- backend/src/config/setupTestEnv.js
+++ backend/src/config/setupTestEnv.js
@@ -1,4 +1,5 @@
 const userRepo = require('./../repositories/userRepository')
+const projectRepo = require('./../repositories/projectRepository')
 
 // emptyDBs :: _ -> [Promise Query]
 const emptyDBs = () => [
@@ -11,13 +12,28 @@ const emptyDBs = () => [
 const saveUsers = () =>
   [
     { username: 'existing', password: 'password' },
-    { username: 'john', password: 'password' },
+    { username: 'ringo', password: 'password' },
     { username: 'paul', password: 'password' }
   ].map(userRepo.save)
+
+const head = ([x, ...xs]) => x
+
+const createProject = user =>
+  projectRepo
+    .create({
+      title: 'My existing project',
+      members: [user.username],
+      deadline: null,
+      description: 'Describing my project',
+      tags: ['test', 'project']
+    })
+    .then(project => userRepo.addProject(user.username, project._id))
 
 module.exports = () =>
   Promise.all(emptyDBs())
     .then(saveUsers)
+    .then(head)
+    .then(createProject)
     .catch(_ => {
       console.log('Test env could not be configured.')
       console.log('Is MongoDB running?')
