b8cff2a6e413fb01984921e8494a7eb6f7171272
antonstrand <jag@antonstrand.se>
2018-05-14 10:59:51 +0200

    Add changeStatus as an action

--- backend/src/routes/socketRouter.js
+++ backend/src/routes/socketRouter.js
@@ -2,11 +2,12 @@ const project = require('./../controllers/project')
 const user = require('./../controllers/user')
 const { switchCase } = require('./../utils')
 
-// actions :: String -> (io, socket, a -> *)
+// actions :: String -> (io, socket, a) -> *
 const actions = switchCase({
+  'ws/USER_AUTHENTICATED': user.initConnection,
   'ws/CREATE_NEW_PROJECT': project.create,
   'ws/CREATE_NEW_TASK': project.addTask,
-  'ws/USER_AUTHENTICATED': user.initConnection
+  'ws/CHANGE_TASK_STATUS': project.changeStatus
 })(() => {})
 
 module.exports = io =>
