2a7fd48788f5cf02309272663be680d4481976cd
antonstrand <jag@antonstrand.se>
2018-05-14 17:59:22 +0200

    Dispatch changed status

--- frontend/src/actions/task.js
+++ frontend/src/actions/task.js
@@ -1,4 +1,4 @@
-import { CREATE_NEW_TASK } from './types'
+import { CREATE_NEW_TASK, CHANGE_TASK_STATUS } from './types'
 
 // createTask :: String -> String -> String -> Action
 export const createTask = (token, parent, taskName) => ({
@@ -8,4 +8,11 @@ export const createTask = (token, parent, taskName) => ({
     parent,
     taskName
   }
+})
+
+export const changeTaskStatus = (token, status, taskId) => ({
+  type: CHANGE_TASK_STATUS,
+  token,
+  status,
+  taskId
 })
