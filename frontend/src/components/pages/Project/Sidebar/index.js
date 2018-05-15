f02997c074914d2d69f48d90a235ce60acd55276
antonstrand <jag@antonstrand.se>
2018-05-15 17:05:51 +0200

    Sidebar

---
+++ frontend/src/components/pages/Project/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const backgroundStyle = {
+  background: '#FFFFFF',
+  boxShadow:
+    '0 2px 4px 0 rgba(99,59,187,0.04), 0 5px 7px 0 rgba(0,0,0,0.02), 0 2px 2px 0 rgba(0,0,0,0.00)'
+}
+
+const Sidebar = () => {
+  return <div style={backgroundStyle} />
+}
+
+export default Sidebar
