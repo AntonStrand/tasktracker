5c81194fc72a898aac65e1384dc53694a547eaa8
antonstrand <jag@antonstrand.se>
2018-05-15 17:02:27 +0200

    Foundation of a sidebar

--- frontend/src/components/pages/Project/index.js
+++ frontend/src/components/pages/Project/index.js
@@ -2,18 +2,24 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import ListView from './ListView'
+import Sidebar from './Sidebar'
+import Header from './../../gui/Header'
 
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   display: grid;
   text-align: left;
-  padding: 0 3em;
   box-sizing: border-box;
+  grid-template-columns: 2fr 6fr;
+  grid-template-rows: 72px auto;
+  overflow: hidden;
 `
 
 const Project = props => (
   <Wrapper>
+    <Header />
+    <Sidebar />
     <ListView {...props} />
   </Wrapper>
 )
