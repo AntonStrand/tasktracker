afa4bf13be874ea63d8af23fa62db0e914e51729
antonstrand <jag@antonstrand.se>
2018-05-16 16:43:48 +0200

    Change header background colour

--- frontend/src/components/gui/Header/index.js
+++ frontend/src/components/gui/Header/index.js
@@ -3,10 +3,13 @@ import logo from './task-tracker-logo.png'
 import styled from 'styled-components'
 
 const Background = styled.div`
-  background-image: linear-gradient(-180deg, #633cbc 0%, #814ac4 100%);
+  background: #895fad;
+  background-image: linear-gradient(22deg, #733aae 0%, #9957ac 100%);
   grid-area: 1/1/2/3;
   padding: 1em 2em;
-  line-height: 3.3em;
+  line-height: 2.3em;
+  text-align: left;
+  max-height: 1.5em;
 `
 
 const Logo = styled.img`
