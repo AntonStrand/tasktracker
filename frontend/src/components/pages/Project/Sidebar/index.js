ac90b6bf3320bd7f604fde231ea7c321d5818e7a
antonstrand <jag@antonstrand.se>
2018-05-16 16:47:39 +0200

    List all projects

--- frontend/src/components/pages/Project/Sidebar/index.js
+++ frontend/src/components/pages/Project/Sidebar/index.js
@@ -1,13 +1,57 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { getProjects } from './../selectors'
+import { connect } from 'react-redux'
+import { Button } from './../../../form/gui'
 
-const backgroundStyle = {
-  background: '#FFFFFF',
-  boxShadow:
-    '0 2px 4px 0 rgba(99,59,187,0.04), 0 5px 7px 0 rgba(0,0,0,0.02), 0 2px 2px 0 rgba(0,0,0,0.00)'
+const Background = styled.div`
+  background: #ffffff;
+  overflow-y: scroll;
+  box-shadow: 0 2px 4px 0 rgba(99, 59, 187, 0.04),
+    0 5px 7px 0 rgba(0, 0, 0, 0.02), 0 2px 2px 0 rgba(0, 0, 0, 0);
+`
+
+const ListItem = styled(Link)`
+  text-decoration: none;
+  font-weight: 600;
+  display: block;
+  padding: 0.8em;
+  width: auto;
+  color: ${props => (props.active ? '#371952' : '#73657f')};
+  ${props => props.active && 'border-left: 0.3em solid #7f4ac4'};
+  transition: all 300ms;
+`
+
+const ButtonWrapper = styled(Link)`
+  display: block;
+  padding: 0.5em 1em;
+`
+
+// const ListItem = ({ children }) => <li>{...children}</li>
+
+const Sidebar = ({ projects, activeProject }) => {
+  return (
+    <Background>
+      <ButtonWrapper to='/dashboard'>
+        <Button primary fullWidth>
+          Create a new project
+        </Button>
+      </ButtonWrapper>
+      {getProjects(projects).map(
+        (project, key) =>
+          console.log(project.id) || (
+            <ListItem to={project.id} active={project.id === activeProject.id}>
+              {project.title}
+            </ListItem>
+          )
+      )}
+    </Background>
+  )
 }
 
-const Sidebar = () => {
-  return <div style={backgroundStyle} />
-}
+const mapToProps = state => ({
+  projects: state.projects
+})
 
-export default Sidebar
+export default connect(mapToProps)(Sidebar)
