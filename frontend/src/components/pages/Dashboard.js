c8be4028758db393961082b83b072acfa6b29040
antonstrand <jag@antonstrand.se>
2018-05-17 02:24:08 +0200

    Redirect if a new project has been created

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { logOut } from './../../actions/user'
 import Form from './../form/ProjectForm'
-import { Button } from './../form/gui'
 import Header from './../gui/Header'
+import { Redirect } from 'react-router-dom'
+import { getProjects } from './../pages/Project/selectors'
 
-const Dashboard = ({ name, logOut, projects }) => (
-  <div>
-    <Header />
-    <div
-      style={{
-        display: 'block',
-        borderBottom: '1px solid #efefef',
-        paddingBottom: '1em'
-      }}
-    >
-      <h1>Welcome, {name}</h1>
-      <Button onClick={logOut}>Log out</Button>
-    </div>
-    <Form />
-  </div>
-)
+class Dashboard extends Component {
+  state = { numOfProjects: this.props.numOfProjects }
 
-const mapToProps = state =>
-  console.log(state) || {
-    name: state.user.username,
-    projects: state.projects
+  render () {
+    const { name, numOfProjects } = this.props
+    const last = numOfProjects - 1
+    return this.state.numOfProjects === numOfProjects ? (
+      <div>
+        <Header />
+        <div
+          style={{
+            display: 'block',
+            borderBottom: '1px solid #efefef',
+            paddingBottom: '1em'
+          }}
+        >
+          <h1>Welcome, {name}</h1>
+        </div>
+        <Form />
+      </div>
+    ) : (
+      <Redirect to={`project/${this.props.projects[last].id}`} />
+    )
   }
+}
+
+const mapToProps = state => ({
+  name: state.user.username,
+  numOfProjects: state.projects.count,
+  projects: getProjects(state.projects)
+})
 
 const mapToDispatch = dispatch => ({
   logOut: () => dispatch(logOut())
@@ -37,6 +47,6 @@ export default connect(mapToProps, mapToDispatch)(Dashboard)
 
 Dashboard.propTypes = {
   name: PropTypes.string.isRequired,
-  logOut: PropTypes.func.isRequired,
+  numOfProjects: PropTypes.number.isRequired,
   projects: PropTypes.object.isRequired
 }
