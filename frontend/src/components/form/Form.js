ea8cb982add39e8587d85586512d3ed7adb9164e
antonstrand <jag@antonstrand.se>
2018-05-17 02:21:30 +0200

    Resetting form after leaving the page

--- frontend/src/components/form/Form.js
+++ frontend/src/components/form/Form.js
@@ -2,12 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import equals from 'ramda/src/equals'
 import { FormField, Button, Message } from './gui'
+import * as R from 'ramda'
+
+// updateItemAtIndex :: Number -> a -> [a] -> [a]
+const updateItemAtIndex = (index, item, arr) => {
+  return [...arr.slice(0, index), item, ...arr.slice(index + 1)]
+}
+
+// setValue :: a -> {value: a} -> {value: a}
+const setValue = R.set(R.lensProp('value'))
 
 class Form extends React.Component {
   state = { fields: this.props.fields }
 
-  updateField = key => ({ target: t }) =>
-    this.setState(({ fields }) => [...fields, (fields[key].value = t.value)])
+  updateField = index => ({ target: t }) =>
+    this.setState(({ fields }) => ({
+      fields: updateItemAtIndex(index, setValue(t.value, fields[index]), fields)
+    }))
 
   validate = () => {
     const noErrors = this.state.fields.map(data => ({ ...data, error: null }))
@@ -30,7 +41,9 @@ class Form extends React.Component {
         <form
           onSubmit={evt => {
             evt.preventDefault()
-            this.validate() && onSubmit(token, this.state.fields)
+            if (this.validate()) {
+              onSubmit(token, fields)
+            }
           }}
         >
           {fields.map(({ name, validate, ...props }, key) => (
